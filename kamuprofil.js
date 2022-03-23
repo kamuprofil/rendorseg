61fd28237bee9a60ca458ef017b0b873be6deb6a
György Kovács <pktzerus@gmail.com>
2022-03-23 05:48:55 +0100

    Lookup-based processing

--- kamuprofil.js
+++ kamuprofil.js
@@ -1,10 +1,19 @@
 document.addEventListener("click", addCommentLabels);
 document.addEventListener("load",  addCommentLabels, true);
 
-let fakeAccounts = [];
+/** Converts an array of usernames into a dictionary usable for direct lookup. */
+function toLookup(list) {
+    const lookup = {};
+    for (const value of list) {
+        lookup[value] = true;
+    }
+    return lookup;
+}
+
+let fakeAccounts = {};
 
 async function init() {
-    fakeAccounts = await getList();
+    fakeAccounts = toLookup(await getList());
     addCommentLabels();
 }
 
@@ -50,29 +59,60 @@ const fakeLabel = {
 };
 const labelTemplate = createLabelElement(fakeLabel);
 
-function addCommentLabels() {
-    for (const name of fakeAccounts) {
-        const commentSelector = 'div ul div[role=article]';
+const profileUrlPattern = /^https:\/\/www\.facebook\.com\/profile\.php\?id=(\d+)/;
+const aliasUrlPattern = /^https:\/\/www\.facebook\.com\/([a-zA-Z0-9.]+)/;
+/** Takes a link and returns the account name / identifier part from it */
+function extractAccountName(url) {
+    // ID-based profile links
+    const matches = url.match(profileUrlPattern);
+    if (matches) {
+        return matches[1];
+    }
+    // Name-based profile links
+    const aliasMatches = url.match(aliasUrlPattern);
+    if (aliasMatches) {
+        return aliasMatches[1];
+    }
+    return null;
+}
 
-        // Add label to author names
-        const commentAuthors = document.querySelectorAll(`${commentSelector} a[href*="${name}"] > span`);
-        for (const commentAuthor of commentAuthors) {
-            const comment = commentAuthor.parentNode;
-            const hasLabel = comment.getAttribute('data-has-label');
-            if (!hasLabel) {
-                comment.setAttribute('data-has-label', 'fake');
+/** Adds labels to all new elements.
+ *
+ * Elements that have already been processed get a 'data-has-label' attribute, and are ignored afterwards.
+ * The relevant user info is extracted from the link an anchor element points to, and the user's corresponding label
+ * is applied (if any).
+ */
+function addCommentLabels() {
+    const commentSelector = 'div ul div[role=article]';
+
+    // Add label to author names
+    // TODO: Try to exclude links to comments here already
+    const commentAuthors = document.querySelectorAll(`${commentSelector} a[href]:not([data-has-label]) > span`);
+    for (const commentAuthor of commentAuthors) {
+        const comment = commentAuthor.parentNode;
+        const hasLabel = comment.getAttribute('data-has-label');
+        if (!hasLabel) {
+            const accountName = extractAccountName(comment.getAttribute('href'));
+            const needsLabel = fakeAccounts[accountName];
+            // Mark element as processed, even if unlabeled
+            comment.setAttribute('data-has-label', needsLabel ? 'fake' : 'null');
+            if (needsLabel) {
                 const label = labelTemplate.cloneNode(true);
                 comment.prepend(label);
             }
         }
+    }
 
-        // Add highlight to author images
-        const commentImages = document.querySelectorAll(`${commentSelector} a[href*="${name}"] > div`);
-        for (const commentImage of commentImages) {
-            const anchor = commentImage.parentNode;
-            const hasLabel = anchor.getAttribute('data-has-label');
-            if (!hasLabel) {
-                anchor.setAttribute('data-has-label', 'fake');
+    // Add highlight to author images
+    const commentImages = document.querySelectorAll(`${commentSelector} a[href]:not([data-has-label]) > div`);
+    for (const commentImage of commentImages) {
+        const anchor = commentImage.parentNode;
+        const hasLabel = anchor.getAttribute('data-has-label');
+        if (!hasLabel) {
+            const accountName = extractAccountName(anchor.getAttribute('href'));
+            const needsLabel = fakeAccounts[accountName];
+            anchor.setAttribute('data-has-label', needsLabel ? 'fake' : 'null');
+            if (needsLabel) {
                 addImageBorder(anchor, fakeLabel)
             }
         }
